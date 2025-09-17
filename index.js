32ff714c16b39227fd7f396b7a545250634f98c0
Francisco Oscar Ballerio <fballerio@ciber.mil>
2025-09-17 10:35:26 -0300

    Cache middleware

--- index.js
+++ index.js
@@ -32,6 +32,23 @@ const allowedRoutes = ['products', 'users', 'posts'];
 const serverTarget = argv.target;
 
 app.use(responseTime());
+const cacheMiddleware = async (req, res, next) => {
+    const { route } = req.params;
+
+    if (allowedRoutes.includes(route)) {
+        // verify is the request is cached
+        const cachedData = await redisClient.get(route)
+
+        console.log(`Cache hit for ${route}`);
+        return res
+                  .header('X-Cache', 'HIT')
+                  .json(JSON.parse(cachedData));
+    } else {
+        res.header('X-Cache', 'MISS')
+        next()
+    }
+}
+    
 
 app.get('/', (req, res) => {
   res.send('Send a request to this urls:\n\t- Products: /products');
@@ -42,45 +59,33 @@ app.get('/clear-cache', async (req, res) => {
     res.send('Cache cleared');
 });
 
-app.get('/:route', async (req, res) => {
+app.get('/:route', cacheMiddleware, async (req, res) => {
     const { route } = req.params;
 
     if (!allowedRoutes.includes(route)) {
         return res.status(400).send('Invalid route. Allowed routes are: ' + allowedRoutes.join(', '));
     } 
 
-    // verify is the request is cached
-    const cachedData = await redisClient.get(route)
+    fetch(`${serverTarget}/${route}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
 
-    if (!cachedData) {
-        fetch(`${serverTarget}/${route}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
+        return response.json();
+    })
+    .then(async (data) => {
 
-            return response.json();
-        })
-        .then(async (data) => {
+        await redisClient.set(route, JSON.stringify(data), 'EX', 300);
 
-            await redisClient.set(route, JSON.stringify(data), 'EX', 300);
-
-            res
-            .header('X-Cache', 'MISS')
-            .json(data);
-        })
-        .catch(error => {
-            console.error(`Error fetching ${route} : ${error}`);
-            res.status(500).send(`Error fetching ${route}`);
-        });        
-    } else {
-        console.log(`Cache hit for ${route}`);
-        return res
-                  .header('X-Cache', 'HIT')
-                  .json(JSON.parse(cachedData));
-    }
+        res
+        .json(data);
+    })
+    .catch(error => {
+        console.error(`Error fetching ${route} : ${error}`);
+        res.status(500).send(`Error fetching ${route}`);
+    });        
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
