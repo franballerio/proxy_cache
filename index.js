5c7515745ea4f3b0b2a4cb61e98fde5b7c930264
Francisco Oscar Ballerio <fballerio@ciber.mil>
2025-09-17 10:07:40 -0300

    caching with Redis implemented!

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
-
+import responseTime from 'response-time';
+import { createClient } from 'redis';
 
 
 const argv = yargs(hideBin(process.argv))
@@ -21,35 +22,63 @@ const argv = yargs(hideBin(process.argv))
 
 const app = express();
 const PORT = argv.port;
+const redisClient = createClient({
+    host: 'localhost',
+    port: 6379
+});
+redisClient.connect()
+
 const allowedRoutes = ['products', 'users', 'posts'];
 const serverTarget = argv.target;
+
+app.use(responseTime());
 
 app.get('/', (req, res) => {
   res.send('Send a request to this urls:\n\t- Products: /products');
 });
 
-app.get('/:route', (req, res) => {
+app.get('/clear-cache', async (req, res) => {
+    await redisClient.flushAll();
+    res.send('Cache cleared');
+});
+
+app.get('/:route', async (req, res) => {
     const { route } = req.params;
 
     if (!allowedRoutes.includes(route)) {
         return res.status(400).send('Invalid route. Allowed routes are: ' + allowedRoutes.join(', '));
     } 
 
-    fetch(`${serverTarget}/${route}`)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
+    // verify is the request is cached
+    const cachedData = await redisClient.get(route)
 
-        return response.json();
-    })
-    .then(data => {
-        res.json(data);
-    })
-    .catch(error => {
-        console.error(`Error fetching ${route} : ${error}`);
-        res.status(500).send(`Error fetching ${route}`);
-    });
+    if (!cachedData) {
+        fetch(`${serverTarget}/${route}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+
+            return response.json();
+        })
+        .then(async (data) => {
+
+            await redisClient.set(route, JSON.stringify(data), 'EX', 300);
+
+            res
+            .header('X-Cache', 'MISS')
+            .json(data);
+        })
+        .catch(error => {
+            console.error(`Error fetching ${route} : ${error}`);
+            res.status(500).send(`Error fetching ${route}`);
+        });        
+    } else {
+        console.log(`Cache hit for ${route}`);
+        return res
+                  .header('X-Cache', 'HIT')
+                  .json(JSON.parse(cachedData));
+    }
 });
 
 
